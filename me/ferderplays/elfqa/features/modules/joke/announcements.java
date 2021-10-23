618d72638b1734d7e52994b35d1c81ac5145a322
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-10-23 10:42:03 +0200

    elfqa on da TOP!

---
+++ me/ferderplays/elfqa/features/modules/joke/announcements.java
@@ -0,0 +1,21 @@
+package me.ferderplays.elfqa.features.modules.joke;
+
+public class announcements extends elfqa {
+    public announcements() {
+        super("annoucements","jk lol", GUI.category.joke, true, true, false);
+        String a = private static final Settings long < type1 > = this.setAnnouncement("i placed %placed_blocks% blocks");
+        STring b = private static final Settings long < type2 > = this.setAnnouncement("i ran %distance% blocks");
+    }
+    void enable() {
+        if (c === a) {
+            do {
+                String c = this.sendAnnouncement(a);
+            }
+        }
+        if (c === b) {
+            do {
+                String c = this.sendAnnouncement(b);
+            }
+        }
+    }
+}
